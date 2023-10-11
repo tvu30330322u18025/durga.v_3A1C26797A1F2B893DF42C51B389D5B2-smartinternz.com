72e610c3b22073ada6e5385a4d4f535cf3eecfd4
tvu30330322u18025 <144678265+tvu30330322u18025@users.noreply.github.com>
2023-10-11 11:59:02 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,12 +1,10 @@
-def isLeapYear(year):
-  if(year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    return True
+def fact_rec(n):
+  if n==0 or n==1:
+    return 1
   else:
-    return False
+    return n*fact_rec(n-1) 
 
-year=2012
+number =2
+res =fact_rec(number)
 
-if isLeapYear(year):
-   print('{} is a leap year.'.format (year))
-else:
-  print('{} is not a leap year.'.format (year))
+print("The factorial of {} is {}".format(number, res))
